754e2bd1e501146ae1c576840585b02aea700435
simone-staccone <simone.staccone@virgilio.it>
2023-10-09 00:12:40 +0200

    Empty project

---
+++ src/main/java/com/mycompany/jdbc/example/JDBCExample.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jdbc.example;
+
+/**
+ *
+ * @author stack1
+ */
+public class JDBCExample {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
